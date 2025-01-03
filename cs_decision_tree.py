8b82053a4d099922a64477c92f9338625f8f77bd
James Gary <gmarchesi888@gmail.com>
2025-01-03 13:18:53 -0500

    Got the decision tree to display at a readable size.

--- cs_decision_tree.py
+++ cs_decision_tree.py
@@ -3,8 +3,8 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 
-# df = pandas.read_csv("csmith_data_2.csv")
-df = pandas.read_csv("order-data_2.csv")
+df = pandas.read_csv("csmith_data_2.csv")
+# df = pandas.read_csv("order-data.csv")
 
 
 features = ["Amount", "Years", "PriorRequests", "AcctsOnIP"]
@@ -15,7 +15,9 @@ y = df["Outcome"]
 dtree = DecisionTreeClassifier()
 dtree = dtree.fit(X.values, y.values)
 
-tree.plot_tree(dtree, feature_names=features)
+plt.figure(figsize=(20, 10))
+tree.plot_tree(dtree, feature_names=features, filled=True)
+
 plt.show()
 
 print(dtree.predict([[25, 3, 0, 1]]))
