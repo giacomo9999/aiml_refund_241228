8e12321221d148ec03ed1f3fc929703e48d81402
James Gary <gmarchesi888@gmail.com>
2025-01-05 11:05:35 -0500

    Changed file in decision-tree.py.

--- cs_decision_tree.py
+++ cs_decision_tree.py
@@ -3,8 +3,8 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 
-df = pandas.read_csv("csmith_data_2.csv")
-# df = pandas.read_csv("order-data.csv")
+# df = pandas.read_csv("csmith_data_2.csv")
+df = pandas.read_csv("order-data.csv")
 
 
 features = ["Amount", "Years", "PriorRequests", "AcctsOnIP"]
